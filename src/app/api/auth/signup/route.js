8c783e05680e5afd3844749e4507fac0e1ad2e41
hasan814 <h.mousavi910@gmail.com>
2024-06-03 21:42:01 +0330

    Added SignUp API

---
+++ src/app/api/auth/signup/route.js
@@ -0,0 +1,36 @@
+import User from "@/models/User";
+import connectDB from "@/utils/connectDB";
+import { NextResponse } from "next/server";
+
+export const POST = async (req) => {
+  try {
+    await connectDB();
+
+    const { email, password } = await req.json();
+    console.log({ email, password });
+    if (!email || !password)
+      return NextResponse.json(
+        { error: "لطفا اطلاعات معتبر را وارد کنید" },
+        { status: 422 }
+      );
+
+    const existingUser = await User.findOne({ email });
+    console.log(existingUser);
+    if (existingUser)
+      return NextResponse.json(
+        { error: "این حساب کاربری وجود دارد" },
+        { status: 422 }
+      );
+    const hashedPassword = await hashPassword(password);
+    const newUser = await User.create({ email, password: hashedPassword });
+    console.log(newUser);
+
+    return NextResponse.json({ message: "حساب کاربری ایجاد شد." });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+};
