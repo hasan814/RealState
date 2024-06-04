437163abc7ef421a4e87abaffa029f1c60a3a5ea
hasan814 <h.mousavi910@gmail.com>
2024-06-04 11:19:26 +0330

    Security Dashboard Panel

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,6 +1,12 @@
 import SideBar from "@/layout/Dashboard/SideBar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-const DashBoardLayout = ({ children }) => {
+const DashBoardLayout = async ({ children }) => {
+  // =========== Session ===========
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/signin");
   return <SideBar>{children}</SideBar>;
 };
 
