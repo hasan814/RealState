bde27c778c6010f7f0952e890d67392984917126
hasan814 <h.mousavi910@gmail.com>
2024-06-05 10:50:54 +0330

    Create Patch API

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -9,7 +9,6 @@ export async function POST(req) {
   try {
     await connectDB();
 
-    const body = await req.json();
     const {
       title,
       description,
@@ -21,7 +20,7 @@ export async function POST(req) {
       amenities,
       rules,
       category,
-    } = body;
+    } = await req.json();
 
     const session = await getServerSession(req);
     const email = session.user.email;
@@ -52,7 +51,7 @@ export async function POST(req) {
         { error: "لطفا اطلاعات معتبر را وارد کنید ." },
         { status: 400 }
       );
-    const newProfile = await Profile.create({
+    await Profile.create({
       title,
       description,
       location,
@@ -67,6 +66,83 @@ export async function POST(req) {
     });
     return NextResponse.json(
       { message: "آگهی جدید اضافه شد ." },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است ." },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(req) {
+  try {
+    await connectDB();
+
+    const {
+      _id,
+      title,
+      description,
+      location,
+      phone,
+      realState,
+      price,
+      constructionDate,
+      amenities,
+      rules,
+      category,
+    } = await req.json();
+
+    const session = await getServerSession(req);
+    const email = session.user.email;
+    if (!session)
+      return NextResponse.json(
+        { error: "لطفا وارد حساب کاربری خود شوید ." },
+        { status: 401 }
+      );
+
+    const user = await User.findOne({ email });
+    if (!user)
+      return NextResponse.json(
+        { error: "حساب کاربری یافت نشد ." },
+        { status: 404 }
+      );
+    if (
+      !_id ||
+      !title ||
+      !location ||
+      !description ||
+      !phone ||
+      !realState ||
+      !price ||
+      !constructionDate ||
+      !category
+    )
+      return NextResponse.json(
+        { error: "لطفا اطلاعات معتبر را وارد کنید ." },
+        { status: 400 }
+      );
+    const profile = await Profile.findOne({ _id });
+    if (user._id.equals(profile.userId))
+      return NextResponse.json(
+        { error: "دسترسی شما به این آگهی محدود شده است ." },
+        { status: 403 }
+      );
+    profile.title = title;
+    profile.description = description;
+    profile.location = location;
+    profile.phone = phone;
+    profile.realState = realState;
+    profile.price = price;
+    profile.constructionDate = constructionDate;
+    profile.amenities = amenities;
+    profile.rules = rules;
+    profile.category = category;
+    profile.save();
+    return NextResponse.json(
+      { message: "آگهی با موفقیت ویزایش شد." },
       { status: 201 }
     );
   } catch (error) {
