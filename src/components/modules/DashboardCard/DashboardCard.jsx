b4a3aa86cde8f92a9e342ca6116b39f12368a919
hasan814 <h.mousavi910@gmail.com>
2024-06-05 12:44:12 +0330

    Finished Delete Function and Finished myProfile Page

--- src/components/modules/DashboardCard/DashboardCard.jsx
+++ src/components/modules/DashboardCard/DashboardCard.jsx
@@ -6,6 +6,7 @@ import { FiEdit } from "react-icons/fi";
 import styles from "./DashboardCard.module.css";
 import Card from "@/elements/Card/Card";
 import { useRouter } from "next/navigation";
+import toast, { Toaster } from "react-hot-toast";
 
 const DashboardCard = ({ data }) => {
   // =========== Router ==========
@@ -15,11 +16,24 @@ const DashboardCard = ({ data }) => {
   const editHandler = () => {
     router.push(`/dashboard/my-profiles/${data._id}`);
   };
-  const deleteHandler = () => {};
+
+  const deleteHandler = async () => {
+    const response = await fetch(`/api/profile/delete/${data._id}`, {
+      method: "DELETE",
+    });
+    const deleteData = await response.json();
+    if (deleteData.error) {
+      toast.error(deleteData.error);
+    } else {
+      toast.success(deleteData.message);
+      router.refresh();
+    }
+  };
 
   // =========== Rendering ==========
   return (
     <div className={styles.container}>
+      <Toaster />
       <Card {...data} />
       <div className={styles.main}>
         <button onClick={editHandler}>
